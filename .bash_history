223da020fa5a23bb9b3ef71912507e0619077e79
khwahish <khwahishseth@gmail.com>
2024-05-24 19:52:39 +0530

    a new project

---
+++ .bash_history
@@ -0,0 +1,31 @@
+git--version
+pwd
+git config --global user.name"khwa678"
+git config --global user.email"khwahishseth@gmail.com"
+git config --list
+git congig --global user.name khwahish
+git config --global user.name khwahish 
+git config --global user.email khwahishseth@gmail.com
+git config --global user.email
+hit config 
+git config --global user.name
+code.
+code.
+touch .gitin
+touch .gitignore
+git init
+ls -1art
+git add kkui.html
+git add kkui.html
+git add index.html
+git commit
+git add kkui.html
+git commit 
+git add contact.html
+git init 
+git status
+git add kkui.html
+git remote -v
+git checkout dec9
+git fetch
+touch ,gitignore
---
+++ .vscode/extensions/ms-python.python-2024.7.11371014/noxfile.py
@@ -0,0 +1,72 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import pathlib
+import nox
+import shutil
+import sys
+
+
+@nox.session()
+def install_python_libs(session: nox.Session):
+    requirements = [
+        ("./python_files/lib/python", "./requirements.txt"),
+        (
+            "./python_files/lib/jedilsp",
+            "./python_files/jedilsp_requirements/requirements.txt",
+        ),
+    ]
+    for target, file in requirements:
+        session.install(
+            "-t",
+            target,
+            "--no-cache-dir",
+            "--implementation",
+            "py",
+            "--no-deps",
+            "--require-hashes",
+            "--only-binary",
+            ":all:",
+            "-r",
+            file,
+        )
+
+    session.install("packaging")
+
+    # Download get-pip script
+    session.run(
+        "python",
+        "./python_files/download_get_pip.py",
+        env={"PYTHONPATH": "./python_files/lib/temp"},
+    )
+
+    if pathlib.Path("./python_files/lib/temp").exists():
+        shutil.rmtree("./python_files/lib/temp")
+
+
+@nox.session()
+def native_build(session:nox.Session):
+    with session.cd("./native_locator"):
+        session.run("cargo", "build", "--release", "--package", "python-finder", external=True)
+        if not pathlib.Path(pathlib.Path.cwd() / "bin").exists():
+            pathlib.Path(pathlib.Path.cwd() / "bin").mkdir()
+
+        if not pathlib.Path(pathlib.Path.cwd() / "bin" / ".gitignore").exists():
+            pathlib.Path(pathlib.Path.cwd() / "bin" / ".gitignore").write_text("*\n", encoding="utf-8")
+
+        if sys.platform == "win32":
+            shutil.copy(
+                "./target/release/python-finder.exe",
+                "./bin/python-finder.exe",
+            )
+        else:
+            shutil.copy(
+                "./target/release/python-finder",
+                "./bin/python-finder",
+            )
+
+
+@nox.session()
+def setup_repo(session: nox.Session):
+    install_python_libs(session)
+    native_build(session)
