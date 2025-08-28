4658c1be2b9b89d2bcec5313a5f2488c1f3cd6c3
khwahish <khwahishseth@gmail.com>
2025-08-28 12:45:49 +0530

    first commit

---
+++ c++abesdsa/name.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	//write a prog to read your name and concatenate with the String
+	string name ;
+	cin>>name;
+	string name2;
+	cin>>name2;
+	cout<<name<<name2;
+	
+	return 0;
+	
+	
+
+}
